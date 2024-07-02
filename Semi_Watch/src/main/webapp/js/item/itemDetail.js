9b31a6fca6366cbe8ad6e4ee09263d567951f0da
nime0110 <77383149+nime0110@users.noreply.github.com>
2024-07-02 13:35:20 +0900

    Update itemDetail.js

--- Semi_Watch/src/main/webapp/js/item/itemDetail.js
+++ Semi_Watch/src/main/webapp/js/item/itemDetail.js
@@ -1,8 +1,8 @@
 /*  아래 코드로 사진 변경 가능 */
   $(document).ready(function() {
-   const contextPath = window.location.pathname.substring(0, window.location.pathname.indexOf("/",2)); // 컨텍스트 패스 
-	 let productNo = $("input#productno").val(); //제품번호
-	 loadReviewPage(1); //리뷰 페이지 로드
+   	const contextPath = window.location.pathname.substring(0, window.location.pathname.indexOf("/",2)); // 컨텍스트 패스 
+	let productNo = $("input#productno").val(); //제품번호
+	loadReviewPage(1); //리뷰 페이지 로드
 	  // 숫자만 입력되도록 제한
 	  
         $('#product__quantity').on('input', function() {
@@ -15,7 +15,7 @@
             }
         });
 	 
-  	 $('ul#choice li button img').on('click',function(){
+  	$('ul#choice li button img').on('click',function(){
 	    var i = $(this).attr('src');
 		console.log(i);
 	    $('.image-box img').attr('src',i);
@@ -26,30 +26,31 @@
 	    console.log(button);
 	    return false;
 	    /* css 효과 주기*/
-	  })
+	})
 	  
-	  
-	  //첫번째 content로 애니메이트
-	  //$('html,body').stop().animate({scrollTop: 0},2000);
-	  //메뉴 클릭하면 해당 위치 찾아가기
-	  $('.categori ul li a').on('click',function(){
+	
+	//첫번째 content로 애니메이트
+	//$('html,body').stop().animate({scrollTop: 0},2000);
+	//메뉴 클릭하면 해당 위치 찾아가기
+	$('.categori ul li a').on('click',function(){
 		//-첫째로 몇번째인지 알아야됨
 		let n = $(this).parent().index();
 		//해당 위치 찾아가기
 		let target =  $('.categori').eq(n).offset().top;
 		$('html,body').stop().animate({scrollTop: target},2000);
 		return false;
-	  })
-	  $('.minus-item').click(function() {
-        let quantityInput = $('#product__quantity');
-        let currentValue = parseInt(quantityInput.val());
-        
-        if (currentValue > 1) {
-          quantityInput.val(currentValue - 1);
-        }
-        if(currentValue == 1) {
-        	$(this).prop("disabled"); 
-        }
+	})
+	  
+	$('.minus-item').click(function() {
+	        let quantityInput = $('#product__quantity');
+	        let currentValue = parseInt(quantityInput.val());
+	        
+	        if (currentValue > 1) {
+	          quantityInput.val(currentValue - 1);
+	        }
+	        if(currentValue == 1) {
+	        	$(this).prop("disabled"); 
+	        }
       });
 
       $('.plus-item').click(function() {
@@ -61,9 +62,9 @@
       
       // 선택한 색상 값 가져오기 -----------------------------
       let selectedColor;
-	  $('#color_select').on('change', function() {
-		selectedColor = $(this).val(); //선택한 색상의 값
-	  });
+      $('#color_select').on('change', function() {
+	      selectedColor = $(this).val(); //선택한 색상의 값
+      });
 
       
       
@@ -88,28 +89,28 @@
 		console.log("==selectedColor : " + selectedColor); //색상
 		console.log("==quantity : " + quantity);
 		
-		 $.ajax({
-            type: "POST",
-            url: "addToCartDetailJSON.flex", 
-            data: {"productNo":productNo,
-            	   "selectedColor":selectedColor,
-            	   "quantity":quantity},
-            dataType: "json",
-            success: function(json) {
-                console.log("AJAX 요청 성공:", json);
-             
-                if (json.loginRequired) { //로그인안했을경우
-                    alert(json.message);
-                    location.href = contextPath + "/login/login.flex"; // 로그인 페이지로 이동
-                } else {
-                    let answer = confirm(json.message);
-                    // 장바구니 페이지로 이동 (config)
-                    if(answer == true) {
-						location.href = contextPath + "/item/itemCart.flex"; //장바구니
-					}
-                    
-                    /// ---------------------------------
-                }
+	$.ajax({
+	            type: "POST",
+	            url: "addToCartDetailJSON.flex", 
+	            data: {"productNo":productNo,
+	            	   "selectedColor":selectedColor,
+	            	   "quantity":quantity},
+	            dataType: "json",
+	            success: function(json) {
+	                console.log("AJAX 요청 성공:", json);
+	             
+	                if (json.loginRequired) { //로그인안했을경우
+	                    alert(json.message);
+	                    location.href = contextPath + "/login/login.flex"; // 로그인 페이지로 이동
+	                } else {
+	                    let answer = confirm(json.message);
+	                    // 장바구니 페이지로 이동 (config)
+	                    if(answer == true) {
+							location.href = contextPath + "/item/itemCart.flex"; //장바구니
+						}
+	                    
+	                    /// ---------------------------------
+	                }
              },
             error: function(request, status, error) {
                 alert("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
